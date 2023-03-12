6186811d38f00eaf9a369a6e09263fca01af12b2
Guilherme Martins <55408462+GUIMHARTINS@users.noreply.github.com>
2023-03-12 19:49:55 -0300

    Finalizando curso da alura

--- advinhacao.py
+++ advinhacao.py
@@ -1,14 +1,31 @@
+from random import randint
+
 print("********************************")
 print("Bem vindo ao jogo de advinhação!")
 print("********************************")
 
-numeroSecreto = 42
-tentativasTotais = 3
+numeroSecreto = randint(1,100)
+
+tentativasTotais = 0
+pontos = 1000
 rodada = 1
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+nivel = int(input("Define o nível: "))
+
+if (nivel == 1):
+    tentativasTotais = 20
+elif (nivel == 2):
+    tentativasTotais = 10
+else:
+    tentativasTotais = 5
 while(rodada <= tentativasTotais):
     print("Tentantiva: ", rodada, "de ", tentativasTotais)
     chute = int(input("Digite o seu número: "))
-
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
     acerto = chute == numeroSecreto
     maior  = chute > numeroSecreto
     menor  = chute < numeroSecreto
@@ -16,9 +33,12 @@ while(rodada <= tentativasTotais):
 
     rodada = rodada + 1
     if (acerto):
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos!".format(pontos))
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif(menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
+        pontosPerdidos = abs(numeroSecreto - chute)
+        pontos = pontos -pontosPerdidos
