7fb67c911b2ea389d3193a8918fe506becfbccaf
Guilherme Martins <55408462+GUIMHARTINS@users.noreply.github.com>
2023-03-19 09:16:25 -0300

    iniciando desenvolvimento do jogo da forca

---
+++ forca.py
@@ -0,0 +1,15 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+    print("Fim do jogo")
+
+    while(not enforcou and not acertou):
+        print("Jogando.. ")
+
+if(__name__ == "__main__"):
+    jogar()
